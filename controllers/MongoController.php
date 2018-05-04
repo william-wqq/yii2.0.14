e9cd21ea29717fa53518a8bd9adeeeddfd7369dc
william <william_wqq@163.com>
2018-05-04 10:02:36 +0800

    add update

--- controllers/MongoController.php
+++ controllers/MongoController.php
@@ -5,12 +5,26 @@ use Yii;
 
 class MongoController extends \yii\web\Controller
 {
+    public $collection;
+    public function __construct()
+    {
+        $this->collection = Yii::$app->mongodb->getCollection('country');
+        //parent::__construct($id, $module, $config);
+    }
+
     public function actionAdd()
     {
         //return $this->render('index');
-        $collection = Yii::$app->mongodb->getCollection('country');
-        $collection->insert(['id' => '2', 'name' => '美国']);
 
+        $this->collection->insert(['id' => '2', 'name' => '美国']);
+        echo 'add success';
+
+    }
+
+    public function actionUpdate()
+    {
+        $this->collection->update(['id' => '2'], ['name' => '日本']);
+        echo 'update success';
     }
 
 }
